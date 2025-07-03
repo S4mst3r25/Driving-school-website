72c038a436c69e211dca44039c1663db05f67dc5
S4mst3r25 <77197894+S4mst3r25@users.noreply.github.com>
2025-07-03 11:43:23 +0300

    Make a basic landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import Logo from './assets/logo.png'
+import { motion } from 'framer-motion'
+import './App.css'
 
 function App() {
   return (
     <>
-      <div className="px-4 py-6 w-full drop-shadow-md bg-white">
+      <div className="sticky-navbar px-4 py-6 w-full drop-shadow-md bg-white">
         <div className="flex items-center justify-center">
           <div className="flex items-center space-x-3">
             <img src={Logo} width={190} />
           </div>
         </div>
       </div>
+      <motion.div
+        className="page-bg"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2 }}
+      >
+        <div className="hero-center-text tektur-bold">
+          <h1 className="text-6xl">Най-престижната автошкола във Враца</h1>
+          <p>Научете се да шофирате с увереност в нашите коли BMW</p>
+        </div>
+      </motion.div>
     </>
   )
 }
